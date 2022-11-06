08c44ddcc5dbdc58c443a6ca038583314d6a1852
Carlos <correamillancarlos@gmail.com>
2022-11-05 23:25:32 -0400

    feat: postcss added

--- dist/stories/components/MyLabel.stories.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-declare const _default: ComponentMeta<({ allCaps, color, fontColor, label, size, }: import("../../components").MyLabelProps) => JSX.Element>;
-export default _default;
-export declare const Basic: ComponentStory<({ allCaps, color, fontColor, label, size, }: import("../../components").MyLabelProps) => JSX.Element>;
-export declare const AllCaps: ComponentStory<({ allCaps, color, fontColor, label, size, }: import("../../components").MyLabelProps) => JSX.Element>;
-export declare const Secondary: ComponentStory<({ allCaps, color, fontColor, label, size, }: import("../../components").MyLabelProps) => JSX.Element>;
-export declare const Tertiary: ComponentStory<({ allCaps, color, fontColor, label, size, }: import("../../components").MyLabelProps) => JSX.Element>;
-export declare const CustomColor: ComponentStory<({ allCaps, color, fontColor, label, size, }: import("../../components").MyLabelProps) => JSX.Element>;
